8333d2fba18034b07f76a5bae972f2e5f056c3ed
Mister_Frizzi <lucas.frizzi@opuspac.com.br>
2024-11-18 14:30:10 -0300

    Initial Commit

--- src/obj/Debug/net9.0/Fingertest.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/Fingertest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fingertest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b0bdbabd1079394d356304429a383c74ecd542")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fingertest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fingertest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
